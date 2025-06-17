a07ee04d3f186f0b673411e389773b88c95ebf49
Emilie <emilie.chnvt@gmail.com>
2025-06-17 15:07:28 +0200

    rename file QrCode  added

--- src/Controller/PdfController.php
+++ src/Controller/PdfController.php
@@ -6,6 +6,7 @@ use App\Entity\Proverbe;
 use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class PdfController extends AbstractController
@@ -13,16 +14,32 @@ final class PdfController extends AbstractController
     #[Route('/pdf/{id}', name: 'app_pdf')]
     public function index(Proverbe $proverbe, \Knp\Snappy\Pdf $knpSnappyPdf)
     {
-        $html = $this->renderView('pdf/index.html.twig', [
-            'proverbe' => $proverbe,
-            ]
-        );
+        $html = $this->renderView('pdf/index.html.twig', ['proverbe' => $proverbe]);
         $knpSnappyPdf->setOption('enable-local-file-access', true);
-        // wkhtmltopdf bloque l’accès aux fichiers locaux : images stockées , images encodées en base64 dans le HTML
-        // enable-local-file-access = true, autorises wkhtmltopdf à accéder aux ressources locales
-        return new PdfResponse(
-            $knpSnappyPdf->getOutputFromHtml($html),
-            'QrCode.pdf'
+        //wkhtmltopd bloque l’accès aux fichiers locaux => qrcode.
+        //
+        //autorise accès
+
+        $pdfContent = $knpSnappyPdf->getOutputFromHtml($html); //génère
+
+        return new Response(
+            $pdfContent,
+            200,
+            [
+                'Content-Type' => 'application/pdf',
+                'Content-Disposition' => ResponseHeaderBag::DISPOSITION_INLINE . '; filename="QrCode.pdf"'
+            ] //pas telecharge directement, pdf affiché directement
         );
     }
 }
+
+
+
+
+
+
+
+
+
+
+
